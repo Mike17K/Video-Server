55c6f15d358d3977e9c8c9ab6a08f9b3d8d366f6
unknown <kaipismike1@gmail.com>
2023-04-05 17:34:47 +0300

    semi auto load bar

--- frontend/index.js
+++ frontend/index.js
@@ -11,16 +11,16 @@ socket.on("connect", () => {
 
 
 let videoData = new Uint8Array(); // create an empty array
+let maxLength=1;
 
 socket.on("video-chunk", (res) => {  
   chunk = res.data;
   let startByte = res.bytesFrom;
   let endByte = res.bytesTo;
+  const newData = new Uint8Array(chunk); // create a new array with data to append
 
   if(videoData.length<endByte){
     // append new data to the array
-    const newData = new Uint8Array(chunk); // create a new array with data to append
-    let offset = videoData.length; // calculate the offset to append the new data
     videoUpdated = new Uint8Array(endByte); // resize the array to fit the new data
     videoUpdated.set(videoData, 0); // copy the new data into the array at the specified offset
     videoUpdated.set(newData, startByte); // copy the new data into the array at the specified offset
@@ -29,30 +29,40 @@ socket.on("video-chunk", (res) => {
     videoData.set(newData, startByte);
   }
 
-
   maxLength = res.videoTotalLength;
   timeline.style.setProperty("--loaded", videoData.length/maxLength);
 
-  if(videoData.length>=maxLength){
+    handleLoadMore();
+});
+
+
+function handleLoadMore(){
+  const percent = videoPlayer.currentTime/videoPlayer.duration || 0;
+
+  console.log(videoData.length/maxLength,percent,0.1);
+  if(videoData.length/maxLength-percent<0.2){
+    let asklength = Math.floor(maxLength/10);
+    if(maxLength-videoData.length<asklength){
+      asklength = maxLength-videoData.length;
+    }
+    socket.emit("send-video-chunk",{bytesFrom:videoData.length,bytesTo:videoData.length+asklength});
+    console.log(`asking to: ${(videoData.length+asklength)/maxLength*100}%`);
+  }else{
+    const duration = videoPlayer.currentTime;
     const file = new Blob([videoData], { type: "video/mp4" });
     const url = URL.createObjectURL(file);
     videoPlayer.src = url;
-    videoPlayer.play();
-
-    console.log(videoPlayer.src);
-    return
+    videoPlayer.currentTime = duration;
+    //videoPlayer.play();
+    console.log("play");
   }
+}
 
-  let asklength = Math.floor(maxLength/10);
-  if(maxLength-videoData.length<asklength){
-    asklength = maxLength-videoData.length;
-  }
-  socket.emit("send-video-chunk",{bytesFrom:videoData.length,bytesTo:videoData.length+asklength});
-});
 
+
+  
 const button = document.querySelector(".loader");
 button.addEventListener("click",(event)=>{
-  console.log("asking for video chunk");
-  socket.emit("send-video-chunk",{bytesFrom:0,bytesTo:5000});
+  handleLoadMore();
 });
 
